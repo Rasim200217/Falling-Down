b13536570ece416f5e82b5156439f377c20b458e
unknown <rasim200217@gmail.com>
2022-04-19 13:45:05 +0500

    add player

---
+++ Assets/Scripts/Player Scripts/PlayerBounds.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBounds : MonoBehaviour
+{
+
+    public float MinX = -2.6f, MaxX = 2.6f, MinY = -5.6f;
+
+    private bool _outOfBounds;
+    
+    void Update()
+    {
+        CheckBounds();
+    }
+
+    void CheckBounds()
+    {
+        Vector2 temp = transform.position;
+
+        if (temp.x > MaxX)
+            temp.x = MaxX;
+
+        if (temp.x < MinX)
+            temp.x = MinX;
+
+        transform.position = temp;
+
+        if (temp.y <= MinY)
+        {
+            if (!_outOfBounds)
+            {
+                _outOfBounds = true;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+
+    public float MoveSpeed = 2f;
+    
+    void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+  
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            _rigidbody2D.velocity = new Vector2(MoveSpeed, _rigidbody2D.velocity.y);
+        }
+        
+        if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            _rigidbody2D.velocity = new Vector2(-MoveSpeed, _rigidbody2D.velocity.y);
+        }
+    }
+
+    public void PlatformMove(float x)
+    {
+        _rigidbody2D.velocity = new Vector2(x, _rigidbody2D.velocity.y);
+    }
+}
