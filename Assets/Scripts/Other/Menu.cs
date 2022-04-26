181a3bab5e0528f590abd67cdb4800c5e60ff8d5
unknown <rasim200217@gmail.com>
2022-04-26 19:09:22 +0500

    add

---
+++ Assets/Scripts/Other/Menu.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    private GameObject _panelOptions;
+    
+    private void Start()
+    {
+        _panelOptions = transform.GetChild(4).gameObject;
+        _panelOptions.SetActive(false);
+    }
+
+
+    public void Play()
+    {
+        SceneManager.LoadScene(sceneBuildIndex: 1);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Options()
+    {
+        _panelOptions.SetActive(true);
+    }
+    
+    public void CancelOptions()
+    {
+        _panelOptions.SetActive(false);
+    }
+}
