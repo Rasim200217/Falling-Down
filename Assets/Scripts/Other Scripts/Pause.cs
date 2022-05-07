890fe16b63a83a76d2d3c257190b66f854be5439
unknown <rasim200217@gmail.com>
2022-05-07 13:48:48 +0500

    pause

---
+++ Assets/Scripts/Other Scripts/Pause.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    private GameObject _panelPause;
+
+
+    private void Start()
+    {
+        _panelPause = transform.GetChild(4).gameObject;
+        _panelPause.SetActive(false);
+    }
+
+    public void PauseBtn()
+    {
+        Time.timeScale = 0;
+        _panelPause.SetActive(true);
+    }
+    
+    public void ContinueBtn()
+    {
+        Time.timeScale = 1;
+        _panelPause.SetActive(false);
+    }
+}
