4dd5b616090b7bf2337a4e9f365124f5a75d4bd9
unknown <rasim200217@gmail.com>
2022-05-23 17:11:55 +0500

    add

---
+++ Assets/Scripts/Other Scripts/LevelSound.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSound : MonoBehaviour
+{
+    private static readonly string MusicPref = "MusicPref";
+    
+    private float _musicFloat;
+    public AudioSource MusicAudio;
+
+    private void Awake()
+    {
+        LevelSoundSettings();
+    }
+
+    private void LevelSoundSettings()
+    {
+        _musicFloat = PlayerPrefs.GetFloat(MusicPref);
+
+        MusicAudio.volume = _musicFloat;
+    }
+}
---
+++ Assets/Scripts/Other Scripts/VolumeManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeManager : MonoBehaviour
+{
+   private static readonly string FirstPlay = "FirstPlay";
+   private static readonly string MusicPref = "MusicPref";
+
+   private int _firstPlayInt;
+   public Slider MusicSlider;
+   private float _musicFloat;
+
+   public AudioSource MusicAudio;
+
+
+   private void Start()
+   {
+      _firstPlayInt = PlayerPrefs.GetInt(FirstPlay);
+
+      if (_firstPlayInt == 0)
+      {
+         _musicFloat = 0.25f;
+         MusicSlider.value = _musicFloat;
+         PlayerPrefs.SetFloat(MusicPref, _musicFloat);
+         PlayerPrefs.SetInt(FirstPlay, -1);
+      }
+      else
+      {
+         _musicFloat = PlayerPrefs.GetFloat(MusicPref);
+         MusicSlider.value = _musicFloat;
+      }
+   }
+
+   public void SaveSoundSettings()
+   {
+      PlayerPrefs.SetFloat(MusicPref, MusicSlider.value);
+   }
+
+   private void OnApplicationFocus(bool hasFocus)
+   {
+      if(!hasFocus)
+         SaveSoundSettings();
+   }
+
+   public void UpdateSound()
+   {
+      MusicAudio.volume = MusicSlider.value;
+      
+   }
+}
