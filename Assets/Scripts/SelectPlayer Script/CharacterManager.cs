155fe5a42f7b1eab3696bcb1a3656122ecc191b6
unknown <rasim200217@gmail.com>
2022-06-02 13:54:44 +0500

    skinplayer

---
+++ Assets/Scripts/SelectPlayer Script/CharacterManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CharacterManager : MonoBehaviour
+{
+    public CharacterDatabase CharacterDatabase;
+
+    public Text NameText;
+    public SpriteRenderer ArtworkSprite;
+    
+    private int _selectedOption = 0;
+
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("selectedOption"))
+            _selectedOption = 0;
+        else
+          Load();
+
+        UpdateCharacter(_selectedOption);
+    }
+
+    public void NextOption()
+    {
+        _selectedOption++;
+
+        if (_selectedOption >= CharacterDatabase.CharacterCount)
+            _selectedOption = 0;
+        
+        UpdateCharacter(_selectedOption);
+        Save();
+        
+    }
+
+    public void BackOption()
+    {
+        _selectedOption--;
+
+        if (_selectedOption < 0)
+            _selectedOption = CharacterDatabase.CharacterCount - 1;
+        
+        UpdateCharacter(_selectedOption);
+        Save();
+
+    }
+
+    private void UpdateCharacter(int selectedOption)
+    {
+        Character character = CharacterDatabase.GetCharacter(selectedOption);
+        ArtworkSprite.sprite = character.CharacterSprite;
+        NameText.text = character.CharacterName;
+    }
+
+    private void Load()
+    {
+        _selectedOption = PlayerPrefs.GetInt("selectedOption");
+    }
+    
+    private void Save()
+    {
+        PlayerPrefs.SetInt("selectedOption", _selectedOption);
+    }
+
+    public void ChangeScene(int sceneID)
+    {
+        SceneManager.LoadScene(sceneID);
+    }
+}
---
+++ Assets/Scripts/SelectPlayer Script/SkinPlayer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkinPlayer : MonoBehaviour
+{
+    public CharacterDatabase CharacterDatabase;
+    
+    public SpriteRenderer ArtworkSprite;
+    
+    private int _selectedOption = 0;
+
+
+    private void Start()
+    {
+        if (!PlayerPrefs.HasKey("selectedOption"))
+            _selectedOption = 0;
+        else
+            Load();
+
+        UpdateCharacter(_selectedOption);
+    }
+
+    private void UpdateCharacter(int selectedOption)
+    {
+        Character character = CharacterDatabase.GetCharacter(selectedOption);
+        ArtworkSprite.sprite = character.CharacterSprite;
+    }
+
+    private void Load()
+    {
+        _selectedOption = PlayerPrefs.GetInt("selectedOption");
+    }
+
+}
+
+
